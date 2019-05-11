e5b8df6d0ab1a49ff5826735a6a0219f375d7ed9
lutfiadnan92 <adnanlutfi92@gmail.com>
2019-05-11 09:16:50 +0000

    update js function

--- platforms/browser/www/database/login.php
+++ platforms/browser/www/database/login.php
@@ -1,12 +1,21 @@
 <?php
 include "database.php";
 
-$user = $_POST['user'];
-$pass = $_POST['pass'];
+$user = $_REQUEST['user'];
+$pass = $_REQUEST['pass'];
+// $user = "Admin";
+// $pass = "123";
 
 $query = "select nama,password from pasiens where nama='$user' && password='$pass'";
 $sql = $conn->query($query);
 $row = $sql->fetch_object();
 
-return json_encode(["nama" => $row->nama, "password" => $row->password]);
+// echo json_encode(["nama" => $row->nama, "password" => $row->password]);
+
+if($sql->num_rows == 0){
+	echo 'Cek kembali username dan password anda!';
+}
+else{
+	echo 'ok';
+}
 ?>
--- www/js/my-app.js
+++ www/js/my-app.js
@@ -19,6 +19,10 @@ var app = new Framework7({
     // Add default routesabout
     routes: [
       {
+        path: '/',
+        url: 'index.html',
+      },
+      {
         path: '/about/',
         url: 'about.html',
       },
@@ -61,40 +65,82 @@ function onBackKeyDown(){
   }
 }
 
-$$('#login').on('click', function () {
-  let username = $$('#username').val();
-  let password = $$('#password').val();
-  
-  if (!username || !password){
-   app.dialog.alert("Please submit username dan password!!!","Warning");
-   return;
-  }else{
-    app.request({
-        method: 'GET',
-        url: 'database/login.php',
-        data:{
-          user:username,
-          pass:password
-        },
-        success: function(data) {
-          console.log(data.nama);
-          if(username && password){
-            mainView.router.navigate('/home/');
-          }else{
-            app.dialog.alert("Wrong Username and Password","Information");
+// Option 1. Using one 'page:init' handler for all pages
+$$(document).on('page:init', function (e) {
+  // Do something here when page loaded and initialized
+  console.log(e);
+  const page = e.detail;
+
+  //page home
+  if(page.name === "home"){
+    $$('#login').on('click', function () {
+      const username = $$('#username').val();
+      const password = $$('#password').val();
+
+      if (!username || !password){
+        app.dialog.alert("Please submit username dan password!!!","Warning");
+        return;
+      }else{
+        app.request({
+          method: "POST",
+          dataType: "html",
+          url: "http://armed.atwebpages.com/login.php",
+          data:{
+            user:username,
+            pass:password
+          },
+          success: function(pesan) {
+            console.log(pesan);
+            if(pesan == "ok"){
+              mainView.router.navigate('/home/');
+            }else{
+              app.dialog.alert("Wrong Username and Password","Information");
+            }
+          },
+          error: function(xhr) {
+            app.dialog.alert("Page not found","Information");
+            return;
+          }
+        });
+      }
+    });
+  }
+
+  //page menu utama
+  if(page.name === "home-view"){
+    //logout function
+    $$('#logout').on('click', function () {
+      app.request({
+        method: "POST",
+        dataType: "html",
+        url: "http://armed.atwebpages.com/logout.php",
+        success: function(logout) {
+          if(logout == "true"){
+            mainView.router.navigate('/',{reloadAll : true});
           }
         },
         error: function(xhr) {
           app.dialog.alert("Page not found","Information");
           return;
         }
+      });
     });
   }
-});
 
-// Option 1. Using one 'page:init' handler for all pages
-$$(document).on('page:init', function (e) {
-  // Do something here when page loaded and initialized
-  console.log(e);
-  const page = e.detail;
+  //page pendaftaran
+  if(page.name === "pendaftaran"){
+    //ketika tombol pesan di click
+    $$('#pesan').on('click', function () {
+      const antrian = $$('#nomor-antrian');
+      let terpanggil = 0;
+
+      if(terpanggil === 1000){
+        terpanggil = 1000;
+      }else if(terpanggil <= 1000){
+        terpanggil++;
+      }
+
+      antrian.html(terpanggil);
+    });
+  }
 })
